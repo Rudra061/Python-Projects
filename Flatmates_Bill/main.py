2ed117a3162e90ad0f61689993703bc3cabb2ceb
Rudra061 <58043374+Rudra061@users.noreply.github.com>
2023-12-27 23:38:56 +0530

    added pdf generation functionalty

--- Flatmates_Bill/main.py
+++ Flatmates_Bill/main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     """
         Object that contains data about a bill, such as
@@ -19,5 +21,57 @@ class Flatmates:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        pass
+    def pays(self, bill, flatmate2):
+
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+
+        return to_pay
+
+
+
+
+class PdfReport:
+    """
+        Generates a PDF report showing the amount
+        to be paid by different flatmates
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2),2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1),2))
+
+        pdf = FPDF(orientation="P", unit='pt', format="A4")
+        pdf.add_page()
+
+        # insert title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+        #insert period label and value
+        pdf.cell(w=100, h=40, txt="Period", border=1, align="C")
+        pdf.cell(w=120, h=40, txt=bill.period, border=1, align="C", ln=1)
+
+        #insert the flatmate name and amount to be paid
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1, align="C")
+        pdf.cell(w=120, h=40, txt= flatmate1_pay, border=1, align="C", ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1, align="C")
+        pdf.cell(w=120, h=40, txt=flatmate2_pay, border=1, align="C", ln=1)
+
+        pdf.output(self.filename)
+
+
+the_bill = Bill(amount=120, period="April 2021")
+john = Flatmates(name="John", days_in_house=20)
+marry = Flatmates(name="Marry", days_in_house=25)
+
+print(john.pays(bill=the_bill, flatmate2=marry))
+print(marry.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
